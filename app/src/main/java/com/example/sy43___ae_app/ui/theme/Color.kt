bc990511261507d71762707cb2164482b8fe1ce8
Lofane <165309408+Ghost0mega@users.noreply.github.com>
2026-03-10 17:28:48 +0100

    ae colors

--- app/src/main/java/com/example/sy43___ae_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/sy43___ae_app/ui/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Couleurs AE
+
+val LightBlue = Color(0xFF15608F)
+
+val DarkBlue = Color(0xFF354A5F)
+
+val LightYellow = Color(0xFFFFDD57)
+
+val DarkYellow = Color(0xFF725B00)
+
+val LightBG = Color(0xFFFFFFFF)
+
+val DarkBG = Color(0xFF181A1B)
